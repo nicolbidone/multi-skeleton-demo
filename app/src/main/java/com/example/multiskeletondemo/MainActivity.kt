ef1fc3260a96aa0b65683fd961f3d3ab2f6d4062
Nicolas Lopez Bidone <nicolas.lb@globant.com>
2020-05-08 16:20:13 -0300

    Implemented skeleton elyne

--- app/src/main/java/com/example/multiskeletondemo/MainActivity.kt
+++ app/src/main/java/com/example/multiskeletondemo/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btn_to_elyne.setOnClickListener {
-            startActivity<LoaderViewActivity>()
+            startActivity<ElyneLoaderActivity>()
         }
     }
 }
